0c1271a35a7d64b1030b84e814a56a54bea986e5
evriley <36774521+evriley@users.noreply.github.com>
2018-04-13 16:19:56 -0500

    Add files via upload

---
+++ FinalProject/find.ino
@@ -0,0 +1,27 @@
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+void find()
+{
+ 
+  if(analogRead(E) > analogRead(W))
+     while(analogRead(N) <= analogRead(W) && analogRead(N) <= analogRead(E) && analogRead(N) <= analogRead(S)/*E and W are not almost the same*/)
+      {//rotate right
+        
+      }
+  else
+     while(analogRead(N) <= analogRead(W) && analogRead(N) <= analogRead(E) && analogRead(N) <= analogRead(S)/*E and W are not almost the same*/)
+      {//rotate left
+        
+      }
+
+ 
+}
+
